92cec7201f300aac1b1f88fbf80afddc04e4dd4d
Andrei <andreihandra1@gmail.com>
2023-01-19 22:04:46 +0200

    some done

--- src/HAL9000/src/syscall.c
+++ src/HAL9000/src/syscall.c
@@ -5,6 +5,8 @@
 #include "syscall_func.h"
 #include "syscall_no.h"
 #include "mmu.h"
+#include "thread.h"
+#include "thread_internal.h"
 #include "process_internal.h"
 #include "dmp_cpu.h"
 
@@ -68,6 +70,63 @@ SyscallHandler(
             status = SyscallValidateInterface((SYSCALL_IF_VERSION)*pSyscallParameters);
             break;
         // STUDENT TODO: implement the rest of the syscalls
+        case SyscallIdFileWrite:
+            status = SyscallFileWrite(
+                (UM_HANDLE)pSyscallParameters[0],
+                (PVOID)pSyscallParameters[1],
+                (QWORD)pSyscallParameters[2],
+                (QWORD*)pSyscallParameters[3]
+                );
+            break;
+        case SyscallIdThreadExit:
+            status = SyscallThreadExit((STATUS)*pSyscallParameters);
+            break;
+        case SyscallIdProcessExit:
+            status = SyscallProcessExit((STATUS)*pSyscallParameters);
+            break;
+            /*
+        case SyscallIdThreadCreate:
+            status = SyscallThreadCreate(
+                (PFUNC_ThreadStart)pSyscallParameters[0],
+                (PVOID)pSyscallParameters[1],
+                (UM_HANDLE*)pSyscallParameters[2]);
+            break;
+        case SyscallIdThreadGetTid:
+            status = SyscallThreadGetTid(
+                (UM_HANDLE)pSyscallParameters[0],
+                (TID*)pSyscallParameters[1]);
+            break;
+        case SyscallIdThreadWaitForTermination:
+            status = SyscallThreadWaitForTermination(
+                (UM_HANDLE)pSyscallParameters[0],
+                (STATUS*)pSyscallParameters[1]);
+            break;
+        case SyscallIdThreadCloseHandle:
+            status = SyscallThreadCloseHandle(
+                (UM_HANDLE)pSyscallParameters[0]);
+            break;
+        case SyscallIdProcessCreate:
+            status = SyscallProcessCreate((char*)pSyscallParameters[0], (QWORD)pSyscallParameters[1], (char*)pSyscallParameters[2], (QWORD)pSyscallParameters[3], (UM_HANDLE*)pSyscallParameters[4]);
+            break;
+        case SyscallIdProcessCloseHandle:
+            status = SyscallProcessCloseHandle((UM_HANDLE)pSyscallParameters[0]);
+            break;
+        case SyscallIdProcessGetPid:
+            status = SyscallProcessGetPid((UM_HANDLE)pSyscallParameters[0], (PID*)pSyscallParameters[1]);
+            break;
+        case SyscallIdProcessWaitForTermination:
+            status = SyscallProcessWaitForTermination((UM_HANDLE)pSyscallParameters[0], (STATUS*)pSyscallParameters[1]);
+            break;
+        case SyscallIdFileCreate:
+            status = SyscallFileCreate((char*)pSyscallParameters[0], (QWORD)pSyscallParameters[1], (BOOLEAN)pSyscallParameters[2], (BOOLEAN)pSyscallParameters[3], (UM_HANDLE*)pSyscallParameters[4]);
+            break;
+        case SyscallIdFileRead:
+            status = SyscallFileRead((UM_HANDLE)pSyscallParameters[0], (PVOID)pSyscallParameters[1], (QWORD)pSyscallParameters[2], (QWORD*)pSyscallParameters[3]);
+            break;
+        case SyscallIdFileClose:
+            status = SyscallFileClose((UM_HANDLE)pSyscallParameters[0]);
+            break;
+            */
         default:
             LOG_ERROR("Unimplemented syscall called from User-space!\n");
             status = STATUS_UNSUPPORTED;
@@ -170,3 +229,465 @@ SyscallValidateInterface(
 }
 
 // STUDENT TODO: implement the rest of the syscalls
+STATUS
+SyscallFileWrite(
+    IN  UM_HANDLE                   FileHandle,
+    IN_READS_BYTES(BytesToWrite)
+    PVOID                       Buffer,
+    IN  QWORD                       BytesToWrite,
+    OUT QWORD* BytesWritten
+) 
+{
+    if (BytesWritten == NULL) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (FileHandle == UM_FILE_HANDLE_STDOUT) {
+        *BytesWritten = BytesToWrite;
+        LOG("[%s]:[%s]\n", ProcessGetName(NULL), Buffer);
+        return STATUS_SUCCESS;
+    }
+
+    *BytesWritten = BytesToWrite;
+    return STATUS_SUCCESS;
+
+
+}
+STATUS
+SyscallThreadExit(
+    IN STATUS   ExitStatus
+)
+{
+    ThreadExit(ExitStatus);
+    return STATUS_SUCCESS;
+}
+STATUS
+SyscallProcessExit(
+    IN      STATUS                  ExitStatus
+)
+{
+    PPROCESS Process;
+    Process = GetCurrentProcess();
+    Process->TerminationStatus = ExitStatus;
+    ProcessTerminate(Process);
+    return STATUS_SUCCESS;
+}
+
+//threads from project
+/*
+STATUS
+SyscallThreadCreate(
+    IN      PFUNC_ThreadStart       StartFunction,
+    IN_OPT  PVOID                   Context,
+    OUT     UM_HANDLE* ThreadHandle
+) {
+
+    PTHREAD thread;
+
+    if (StartFunction == NULL) {
+        *ThreadHandle = UM_INVALID_HANDLE_VALUE;
+        return STATUS_UNSUCCESSFUL;
+    }
+
+
+    STATUS validationStatus = MmuIsBufferValid((PVOID)StartFunction, sizeof(StartFunction), PAGE_RIGHTS_ALL, GetCurrentProcess());
+
+    if (validationStatus != STATUS_SUCCESS) {
+        *ThreadHandle = UM_INVALID_HANDLE_VALUE;
+        return STATUS_UNSUCCESSFUL;
+    }
+
+
+
+    STATUS createStatus = ThreadCreateEx("name", ThreadPriorityDefault, StartFunction, Context, &thread, GetCurrentProcess());
+
+    if (createStatus == STATUS_SUCCESS) {
+        PPROCESS currentProcess = GetCurrentProcess();
+        INTR_STATE dummyState;
+        PHANDLE_THREAD structure = ExAllocatePoolWithTag(PoolAllocateZeroMemory, sizeof(HANDLE_THREAD), HEAP_HANDLE_TAG, 0);
+        structure->Thread = thread;
+        structure->Handle = handleGen;
+
+        *ThreadHandle = handleGen;
+
+        LockAcquire(&currentProcess->HandleListLock, &dummyState);
+        handleGen++;
+        InsertTailList(&currentProcess->HandleThreadList, &structure->HandleThreadList);
+        LockRelease(&currentProcess->HandleListLock, dummyState);
+    }
+
+    return createStatus;
+}
+STATUS
+SyscallThreadGetTid(
+    IN_OPT  UM_HANDLE               ThreadHandle,
+    OUT     TID* ThreadId
+) {
+
+    PPROCESS currentProcess = GetCurrentProcess();
+    if (ThreadHandle == UM_INVALID_HANDLE_VALUE) {
+        *ThreadId = GetCurrentThread()->Id;
+        return STATUS_SUCCESS;
+    }
+    STATUS validationStatus = MmuIsBufferValid((PVOID)ThreadHandle, sizeof(ThreadHandle), PAGE_RIGHTS_ALL, GetCurrentProcess());
+
+    if (validationStatus != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->HandleListLock, &dummyState);
+    PLIST_ENTRY handleEntryList = currentProcess->HandleThreadList.Flink;
+    while (handleEntryList != &currentProcess->HandleThreadList) {
+        PHANDLE_THREAD handleThread = CONTAINING_RECORD(handleEntryList, HANDLE_THREAD, HandleThreadList);
+        if (handleThread->Handle == ThreadHandle) {
+            *ThreadId = ThreadGetId(handleThread->Thread);
+            LockRelease(&currentProcess->HandleListLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntryList = handleEntryList->Flink;
+    }
+    LockRelease(&currentProcess->HandleListLock, dummyState);
+    return STATUS_UNSUCCESSFUL;
+}
+STATUS
+SyscallThreadWaitForTermination(
+    IN      UM_HANDLE               ThreadHandle,
+    OUT     STATUS* TerminationStatus
+) {
+    PPROCESS currentProcess = GetCurrentProcess();
+    if (ThreadHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    STATUS validationStatus = MmuIsBufferValid((PVOID)ThreadHandle, sizeof(ThreadHandle), PAGE_RIGHTS_READ, GetCurrentProcess());
+
+    if (validationStatus != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->HandleListLock, &dummyState);
+    PLIST_ENTRY handleEntryList = currentProcess->HandleThreadList.Flink;
+    while (handleEntryList != &currentProcess->HandleThreadList) {
+        PHANDLE_THREAD handleThread = CONTAINING_RECORD(handleEntryList, HANDLE_THREAD, HandleThreadList);
+        if (handleThread->Handle == ThreadHandle) {
+            ThreadWaitForTermination(handleThread->Thread, TerminationStatus);
+            LockRelease(&currentProcess->HandleListLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntryList = handleEntryList->Flink;
+    }
+    LockRelease(&currentProcess->HandleListLock, dummyState);
+    return STATUS_UNSUCCESSFUL;
+}
+
+STATUS
+SyscallThreadCloseHandle(
+    IN      UM_HANDLE               ThreadHandle
+) {
+    PPROCESS currentProcess = GetCurrentProcess();
+    if (ThreadHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    STATUS validationStatus = MmuIsBufferValid((PVOID)ThreadHandle, sizeof(ThreadHandle), PAGE_RIGHTS_ALL, GetCurrentProcess());
+    if (validationStatus != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->HandleListLock, &dummyState);
+    PLIST_ENTRY handleEntryList = currentProcess->HandleThreadList.Flink;
+    while (handleEntryList != &currentProcess->HandleThreadList) {
+        PHANDLE_THREAD handleThread = CONTAINING_RECORD(handleEntryList, HANDLE_THREAD, HandleThreadList);
+        if (handleThread->Handle == ThreadHandle) {
+            ThreadCloseHandle(handleThread->Thread);
+            //INTR_STATE dummyState;
+            //LockAcquire(&currentProcess->HandleListLock, &dummyState);
+            RemoveEntryList(&handleThread->HandleThreadList);
+            LockRelease(&currentProcess->HandleListLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntryList = handleEntryList->Flink;
+    }
+    LockRelease(&currentProcess->HandleListLock, dummyState);
+    return STATUS_UNSUCCESSFUL;
+}
+*/
+
+//from alex
+/*
+STATUS
+SyscallProcessCreate(
+    IN_READS_Z(PathLength)
+    char* ProcessPath,
+    IN          QWORD               PathLength,
+    IN_READS_OPT_Z(ArgLength)
+    char* Arguments,
+    IN          QWORD               ArgLength,
+    OUT         UM_HANDLE* ProcessHandle
+)
+{
+    const char* Partition = IomuGetSystemPartitionPath();
+    char app[14] = "Applications\\";
+    char FinalPath[100];
+    strncpy(FinalPath, Partition, strlen(Partition));
+    strncpy(FinalPath + strlen(Partition), app, strlen(app));
+    strncpy(FinalPath + strlen(Partition) + strlen(app), ProcessPath, strlen(ProcessPath));
+    PPROCESS process = GetCurrentProcess();
+    if (MmuIsBufferValid((PVOID)ProcessPath, PathLength, PAGE_RIGHTS_ALL, process) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    if (PathLength < 0) {
+
+        return STATUS_SUCCESS;
+    }
+    if (ArgLength < 0) {
+
+        return STATUS_SUCCESS;
+    }
+    PPROCESS createdProcess;
+    STATUS status = ProcessCreate(FinalPath, Arguments, &createdProcess);
+    if (status != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    PPROCESS_HANDLE handle = ExAllocatePoolWithTag(PoolAllocateZeroMemory, sizeof(PPROCESS_HANDLE), HEAP_HANDLE_TAG, 0);
+    if (handle == NULL)
+    {
+        return STATUS_HEAP_INSUFFICIENT_RESOURCES;
+    }
+    handle->handle = globalHandle + 0x1;
+    handle->process = createdProcess;
+    INTR_STATE dummyState;
+    if (process != NULL) {
+        LockAcquire(&process->ProcessHandleLock, &dummyState);
+        InsertHeadList(&process->ProcessHandleList, &handle->handleElem);
+        LockRelease(&process->ProcessHandleLock, dummyState);
+    }
+    *ProcessHandle = handle->handle;
+    globalHandle++;
+    return STATUS_SUCCESS;
+}
+
+STATUS
+SyscallProcessGetPid(
+    IN_OPT  UM_HANDLE               ProcessHandle,
+    OUT     PID* ProcessId
+)
+{
+
+    if (ProcessHandle == UM_FILE_HANDLE_STDOUT || ProcessHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_SUCCESS;
+    }
+
+    PPROCESS process = GetCurrentProcess();
+    PLIST_ENTRY handleEntry = process->ProcessHandleList.Flink;
+    INTR_STATE dummyState;
+    LockAcquire(&process->ProcessHandleLock, &dummyState);
+    while (handleEntry != &process->ProcessHandleList) {
+        PROCESS_HANDLE* pHandle = CONTAINING_RECORD(handleEntry, PROCESS_HANDLE, handleElem);
+        if (pHandle->handle == ProcessHandle) {
+            *ProcessId = ProcessGetId(pHandle->process);
+            return STATUS_SUCCESS;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+    LockRelease(&process->ProcessHandleLock, dummyState);
+    return STATUS_SUCCESS;
+}
+
+STATUS
+SyscallProcessWaitForTermination(
+    IN      UM_HANDLE               ProcessHandle,
+    OUT     STATUS* TerminationStatus
+)
+{
+    if (ProcessHandle == UM_FILE_HANDLE_STDOUT || ProcessHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_SUCCESS;
+    }
+
+    PPROCESS process = GetCurrentProcess();
+    PLIST_ENTRY handleEntry = process->ProcessHandleList.Flink;
+    INTR_STATE dummyState;
+    LockAcquire(&process->ProcessHandleLock, &dummyState);
+    while (handleEntry != &process->ProcessHandleList) {
+        PPROCESS_HANDLE pHandle = CONTAINING_RECORD(handleEntry, PROCESS_HANDLE, handleElem);
+
+        if (pHandle->handle == ProcessHandle) {
+
+            ProcessWaitForTermination(pHandle->process, TerminationStatus);
+            LockRelease(&process->ProcessHandleLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+
+    return STATUS_UNSUCCESSFUL;
+
+}
+
+
+STATUS
+SyscallProcessCloseHandle(
+    IN      UM_HANDLE               ProcessHandle
+)
+{
+    PPROCESS process = GetCurrentProcess();
+    if (ProcessHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_SUCCESS;
+    }
+
+    if (ProcessHandle == UM_FILE_HANDLE_STDOUT) {
+        return STATUS_INVALID_PARAMETER1;
+    }
+
+    PLIST_ENTRY handleEntry = process->ProcessHandleList.Flink;
+    INTR_STATE dummyState;
+    LockAcquire(&process->ProcessHandleLock, &dummyState);
+    while (handleEntry != &process->ProcessHandleList) {
+        PROCESS_HANDLE* pHandle = CONTAINING_RECORD(handleEntry, PROCESS_HANDLE, handleElem);
+        if (pHandle->handle == ProcessHandle) {
+            RemoveEntryList(handleEntry);
+            ProcessCloseHandle(pHandle->process);
+            LockRelease(&process->ProcessHandleLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+    LockRelease(&process->ProcessHandleLock, dummyState);
+    return STATUS_UNSUCCESSFUL;
+}
+STATUS
+SyscallFileCreate(
+    IN_READS_Z(PathLength)
+    char* Path,
+    IN          QWORD                   PathLength,
+    IN          BOOLEAN                 Directory,
+    IN          BOOLEAN                 Create,
+    OUT         UM_HANDLE* FileHandle
+)
+{
+    if (!Path) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (MmuIsBufferValid((PVOID)Path, PathLength, PAGE_RIGHTS_ALL, GetCurrentProcess()) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (strlen(Path) == 0 || strcmp(Path, "") == 0) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    const char* Partition = IomuGetSystemPartitionPath();
+    char FinalPath[100];
+    strncpy(FinalPath, Partition, strlen(Partition));
+    strncpy(FinalPath + strlen(Partition), Path, strlen(Path));
+    if (PathLength < 0) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    PPROCESS currentProcess = GetCurrentProcess();
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->FileHandleLock, &dummyState);
+    PLIST_ENTRY handleEntry = currentProcess->FileHandleList.Flink;
+    while (handleEntry != &currentProcess->FileHandleList) {
+        FILE_HANDLE* pHandle = CONTAINING_RECORD(handleEntry, FILE_HANDLE, handleElem);
+        if (strcmp(pHandle->file->FileName, FinalPath) == 0) {
+            LockRelease(&currentProcess->FileHandleLock, dummyState);
+            return STATUS_UNSUCCESSFUL;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+    LockRelease(&currentProcess->FileHandleLock, dummyState);
+    PFILE_OBJECT file;
+    IoCreateFile(&file, FinalPath, Directory, Create, FALSE);
+    UM_HANDLE handleFile = globalHandle + 0x1;
+    PFILE_HANDLE fileHandler = ExAllocatePoolWithTag(PoolAllocateZeroMemory, sizeof(PFILE_HANDLE), HEAP_HANDLE_TAG, 0);
+    if (fileHandler == NULL)
+    {
+        return STATUS_HEAP_INSUFFICIENT_RESOURCES;
+    }
+    fileHandler->file = file;
+    fileHandler->handle = handleFile;
+    if (currentProcess != NULL) {
+        LockAcquire(&currentProcess->FileHandleLock, &dummyState);
+        InsertHeadList(&currentProcess->FileHandleList, &fileHandler->handleElem);
+        LockRelease(&currentProcess->FileHandleLock, dummyState);
+    }
+    *FileHandle = fileHandler->handle;
+    globalHandle++;
+
+    return STATUS_SUCCESS;
+}
+
+STATUS
+SyscallFileClose(
+    IN          UM_HANDLE               FileHandle
+)
+{
+    if (FileHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_SUCCESS;
+    }
+    if (FileHandle == UM_FILE_HANDLE_STDOUT) {
+        return STATUS_SUCCESS;
+    }
+
+    PPROCESS currentProcess = GetCurrentProcess();
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->FileHandleLock, &dummyState);
+    PLIST_ENTRY handleEntry = currentProcess->FileHandleList.Flink;
+    while (handleEntry != &currentProcess->FileHandleList) {
+        FILE_HANDLE* pHandle = CONTAINING_RECORD(handleEntry, FILE_HANDLE, handleElem);
+        if (pHandle->handle == FileHandle) {
+
+            RemoveEntryList(handleEntry);
+            IoCloseFile(pHandle->file);
+            LockRelease(&currentProcess->FileHandleLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+    LockRelease(&currentProcess->FileHandleLock, dummyState);
+    return STATUS_FILE_NOT_FOUND;
+}
+
+STATUS
+SyscallFileRead(
+    IN  UM_HANDLE                   FileHandle,
+    OUT_WRITES_BYTES(BytesToRead)
+    PVOID                       Buffer,
+    IN  QWORD                       BytesToRead,
+    OUT QWORD* BytesRead
+)
+{
+    if (BytesToRead == 0) {
+        *BytesRead = 0;
+        return STATUS_SUCCESS;
+    }
+    PPROCESS currentProcess = GetCurrentProcess();
+    if (MmuIsBufferValid(Buffer, BytesToRead, PAGE_RIGHTS_READ, currentProcess) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (FileHandle == UM_FILE_HANDLE_STDOUT || FileHandle == UM_INVALID_HANDLE_VALUE) {
+        return STATUS_FILE_NOT_FOUND;
+    }
+
+
+
+    INTR_STATE dummyState;
+    LockAcquire(&currentProcess->FileHandleLock, &dummyState);
+    PLIST_ENTRY handleEntry = currentProcess->FileHandleList.Flink;
+    while (handleEntry != &currentProcess->FileHandleList) {
+        FILE_HANDLE* pHandle = CONTAINING_RECORD(handleEntry, FILE_HANDLE, handleElem);
+        if (pHandle->handle == FileHandle) {
+            IoReadFile(pHandle->file, BytesToRead, 0, Buffer, BytesRead);
+            if (BytesRead == NULL) {
+                LockRelease(&currentProcess->FileHandleLock, dummyState);
+                return STATUS_UNSUCCESSFUL;
+            }
+            LockRelease(&currentProcess->FileHandleLock, dummyState);
+            return STATUS_SUCCESS;
+        }
+        handleEntry = handleEntry->Flink;
+    }
+    LockRelease(&currentProcess->FileHandleLock, dummyState);
+    return STATUS_SUCCESS;
+}
+*/
